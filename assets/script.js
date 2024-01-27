d41341c8b16dc53bb4c50da00cad9eecee36b173
AQ <alexquayle10@gmail.com>
2024-01-27 01:27:37 +0000

    Feature: Takes city and locates the long/lat.

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,31 @@
 $("#search-button").on("click", function (e) {
     // Prevents the default behaviour of the page refreshing
     e.preventDefault();
-    // If the input is a city
     // Assign the search box content to a variable
+    let location = $("#search-input").val();
+    // If the input is a city
+    // Extract the longitude and latitude from the API
+    let query = "http://api.openweathermap.org/geo/1.0/direct?q=" + location + "&limit=5&appid=APIKEY"
+    fetch(query)
+      .then(function (response) {
+        return response.json();
+      }).then(function (data) {
+    let latitude= data[0].lat;
+    let longitude = data[0].lon;
+    console.log(latitude);
+    console.log(longitude);
+  });
+
+
+
+
+
+
+
     // Execute the API call, with the variable included
     // Append to the page the current forecast (Date, Temp, Wind, Humidity)
     // Append to the page the future forecast (Date, icon?, Temp, Wind, Humidity)
-    // Append the city to the history sidebar
+    // If city isn't already on the sidebar
+        // Append the city to the history sidebar
     // Else, return 
 })
