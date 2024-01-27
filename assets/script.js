aebc0e022012dd011d85a709e57063de0b0ef37a
AQ <alexquayle10@gmail.com>
2024-01-27 16:11:11 +0000

    Feature: Appends the current forecast for the given city. In the process, the variables in getWeather have been condensed into one line.

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,16 @@
+const current = $('#today');
+// Makes cityName a global variable
+let cityName;
+
 $("#search-button").on("click", function (e) {
     // Prevents the default behaviour of the page refreshing
     e.preventDefault();
-    // Assign the search box content to a variable
-    let location = $("#search-input").val();
-    // If the input is a city
-    // Extract the longitude and latitude from the API
-    let geoQuery = "http://api.openweathermap.org/geo/1.0/direct?q=" + location + "&limit=5&appid=API"
+    // Assigns the search box content to a variable
+    cityName = $("#search-input").val();
+    // TODO: If the input is a city
+    // Extracts the longitude and latitude using the search input
+    let geoQuery = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=API"
+    // API call that retrieves and stores the longitude/latitude for use in the getWeather function
     fetch(geoQuery)
         .then(function (response) {
             return response.json();
@@ -14,10 +19,11 @@ $("#search-button").on("click", function (e) {
             let longitude = data[0].lon;
             getWeather(latitude, longitude);
         });
+
 })
 
 function getWeather(latitude, longitude) {
-    // Execute the API call, with the variable included
+    // Utilises the longitude/latiude variables to access the city's weather
     let query = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=API";
     fetch(query)
         .then(function (response) {
@@ -25,12 +31,10 @@ function getWeather(latitude, longitude) {
         }).then(function (data) {
             console.log(query);
             let temp = data.list[0].main.temp;
-            toCelsius(temp);
-            let date = dayjs().format('D/M/YYYY');
-            let wind = data.list[0].wind.speed;
-            let humidity = data.list[0].main.humidity;
+            // Appends the city name, date, temperature, wind speed, and humidity to the today's forecast container
+            let currentForecast = $(`<h2>${cityName} ${dayjs().format('D/M/YYYY')}</h2><p>${toCelsius(temp)} &deg;C</p><p>${data.list[0].wind.speed} KPH</p><p>${data.list[0].main.humidity}%</p>`);
+            $(current).append(currentForecast);
         });
-    // Append to the page the current forecast (Date, Temp, Wind, Humidity)
     // Append to the page the future forecast (Date, icon?, Temp, Wind, Humidity)
     // If city isn't already on the sidebar
     // Append the city to the history sidebar
