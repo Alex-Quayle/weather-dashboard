90dfd6ff9bf41bd42c3f5b6a0671a5cfc0248cc1
AQ <alexquayle10@gmail.com>
2024-01-27 21:09:04 +0000

    Added error prevention for incorrect city name/no name and avoiding duplicate cities in the sidebar.

--- assets/script.js
+++ assets/script.js
@@ -8,20 +8,27 @@ let previousCities;
 $("#search-button").on("click", function (e) {
     // Prevents the default behaviour of the page refreshing
     e.preventDefault();
+    // Clears any existing current and future forecasts on the page
+    $(current).empty();
+    $(future).empty();
     // Assigns the search box content to a variable
     cityName = $("#search-input").val();
-    // TODO: If the input is a city
     getCoordinates(cityName);
 })
 
 function getCoordinates(cityName) {
     // Extracts the longitude and latitude using the search input
     let geoQuery = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid="
-    // API call that retrieves and stores the longitude/latitude for use in the getWeather function
+    // API call that retrieves and stores the latitude/longitude for use in the getWeather function
     fetch(geoQuery)
         .then(function (response) {
             return response.json();
         }).then(function (data) {
+            // Checks whether the city's latitude/longitude was found
+            if (data.length === 0) {
+                alert("Please enter a valid city name (check the spelling!)");
+                return;
+            }
             let latitude = data[0].lat;
             let longitude = data[0].lon;
             getWeather(latitude, longitude);
@@ -44,23 +51,23 @@ function getWeather(latitude, longitude) {
                 futureForecast = $(`<div><h4>${dayjs().format('D/M/YYYY')}</h4><p>Temp: ${toCelsius(data.list[i].main.temp)} &deg;C</p><p>Wind: ${data.list[i].wind.speed} KPH</p><p>Humidity: ${data.list[i].main.humidity} %</p></div>`);
                 $(future).append(futureForecast);
             }
-            // If the city isn't already on the sidebar
-            previousCities = $(`<button type="submit">${cityName}</button>`)
-            $(sidebar).append(previousCities);
+            // Checks to see if the city currently exists in the sidebar
+            if ($(sidebar).find(`button:contains(${cityName})`).length === 0) {
+                // Creates and appends a button with the city name to the sidebar
+                previousCities = $(`<button type="submit">${cityName}</button>`)
+                $(sidebar).append(previousCities);
+            }
         })
 }
 
 $(sidebar).on('click', 'button', function (e) {
     e.preventDefault();
-    let cityName = $(this).text();
+    $(current).empty();
+    $(future).empty();
+    // Assigns the cityName to the text of the clicked button
+    cityName = $(this).text();
     getCoordinates(cityName);
-    console.log("hello world");
 });
-// If city isn't already on the sidebar
-// let previousCity = $(`<btn></btn>`)
-// $(sidebar).append(previousCity);
-// Append the city to the history sidebar
-// Else, return 
 
 function toCelsius(temp) {
     // Celsius = Kelvin - 273.15
