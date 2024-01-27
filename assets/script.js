94cd5bcbcae56aa6c290e7228f99ad9b7399af50
AQ <alexquayle10@gmail.com>
2024-01-27 18:50:03 +0000

    Resolved temperature returning more than 2 decimal places.

--- assets/script.js
+++ assets/script.js
@@ -32,7 +32,7 @@ function getWeather(latitude, longitude) {
             console.log(query);
             let temp = data.list[0].main.temp;
             // Appends the city name, date, temperature, wind speed, and humidity to the today's forecast container
-            let currentForecast = $(`<h2>${cityName} ${dayjs().format('D/M/YYYY')}</h2><p>${toCelsius(temp)} &deg;C</p><p>${data.list[0].wind.speed} KPH</p><p>${data.list[0].main.humidity}%</p>`);
+            let currentForecast = $(`<h2>${cityName} (${dayjs().format('D/M/YYYY')})</h2><p>Temp: ${toCelsius(temp)} &deg;C</p><p>Wind: ${data.list[0].wind.speed} KPH</p><p>Humidity: ${data.list[0].main.humidity}%</p>`);
             $(current).append(currentForecast);
         });
     // Append to the page the future forecast (Date, icon?, Temp, Wind, Humidity)
@@ -44,5 +44,5 @@ function getWeather(latitude, longitude) {
 function toCelsius(temp) {
     // Celsius = Kelvin - 273.15
     let celsius = temp - 273.15;
-    return celsius;
+    return celsius.toFixed(2);
 }
