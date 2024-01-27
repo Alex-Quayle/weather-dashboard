681d1c116d6a430400c5b7e8dcd66a2e90dedd8d
AQ <alexquayle10@gmail.com>
2024-01-27 01:09:33 +0000

    Added event listener and psuedocode.

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+$("#search-button").on("click", function (e) {
+    // Prevents the default behaviour of the page refreshing
+    e.preventDefault();
+    // If the input is a city
+    // Assign the search box content to a variable
+    // Execute the API call, with the variable included
+    // Append to the page the current forecast (Date, Temp, Wind, Humidity)
+    // Append to the page the future forecast (Date, icon?, Temp, Wind, Humidity)
+    // Append the city to the history sidebar
+    // Else, return 
+})
